1555c363fe760034c79da696f1c28a6955cc8e05
ALong Chai <chaialong@gmail.com>
2016-06-26 17:40:43 +0800

    Spring framework: collection injection

---
+++ spring-framework-reference-4.1.4/src/main/java/example/collection/MergeCollection.java
@@ -0,0 +1,17 @@
+package example.collection;
+
+import java.util.Properties;
+
+public class MergeCollection {
+    public Properties getAdminEmails() {
+        return adminEmails;
+    }
+
+    public void setAdminEmails(Properties adminEmails) {
+        this.adminEmails = adminEmails;
+    }
+
+    private Properties adminEmails;
+
+
+}
